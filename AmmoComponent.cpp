50678244fb966f807949db34c436770bf11ba92c
Nick Jordan <AussieGamersHD@Gmail.com>
2017-03-05 23:33:09 +1100

    Made variables public

---
+++ AmmoComponent.cpp
@@ -0,0 +1,221 @@
+// Change To The Name you chose when you created the project - eg. #include "ShooterGame.h"
+#include "GameName.h"
+#include "AmmoComponent.h"
+
+// Sets default values for this component's properties
+UAmmoComponent::UAmmoComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	//bWantsBeginPlay = false;
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+// Called when the game starts - TODO: Do i need this?
+void UAmmoComponent::BeginPlay()
+{
+	// Check Constructor If Not Working (bWantsBeginPlay)
+	Super::BeginPlay();
+}
+
+bool UAmmoComponent::Fire()
+{
+	bool bSuccess = true; // Assume true by default
+	if(!IsAmmoDepleted())
+	{
+		if(bShootingInteruptsReload && IsReloading())
+		{
+			CancelReload();
+		}
+		else
+		{
+			TakeAmmo(BulletsPerShot);
+			bSuccess = true;
+		}
+	}
+	else
+	{
+		if(FiringWithNoBulletsStartsReload && CanReload())
+		{
+			Reload();
+		}
+		bSuccess = false;
+	}
+	return bSuccess; // return if shot should be fired or not
+}
+
+void UAmmoComponent::Reload()
+{
+	if(NeedsAndCanReload())
+	{
+		StartReloading();
+	}
+}
+
+bool UAmmoComponent::IsReloading()
+{
+	return (bIsReloading && GetWorld()->GetTimerManager().IsTimerActive(ReloadingHandle));
+}
+
+void UAmmoComponent::CancelReload()
+{
+	if(IsReloading())
+	{
+		bIsReloading = false;
+		GetWorld()->GetTimerManager().ClearTimer(ReloadingHandle);
+	}
+}
+
+int32 UAmmoComponent::TakeAmmo(int32 Amount)
+{
+	return SetAmmo(Ammo-Amount);
+}
+
+int32 UAmmoComponent::GiveAmmo(int32 Amount)
+{
+	return SetAmmo(Ammo+Amount);
+}
+
+int32 UAmmoComponent::TakeReserveAmmo()
+{
+	return SetReserveAmmo(ReserveAmmo-1);
+}
+
+int32 UAmmoComponent::GiveReserveAmmo(int32 Amount)
+{
+	return SetReserveAmmo(ReserveAmmo+Amount);
+}
+
+int32 UAmmoComponent::SetAmmo(int32 NewAmmo)
+{
+	Ammo = FMath::Clamp(NewAmmo, 0, MaxAmmo);
+
+	if(Ammo <= 0)
+	{
+		OnAmmoDepleted.Broadcast();
+	}
+
+	return Ammo;
+}
+
+int32 UAmmoComponent::SetReserveAmmo(int32 NewReserveAmmo)
+{
+	ReserveAmmo = FMath::Clamp(NewReserveAmmo, 0, MaxReserveAmmo);
+
+	if(ReserveAmmo <= 0)
+	{
+		OnReserveDepleted.Broadcast();
+	}
+
+	return ReserveAmmo;
+}
+
+int32 UAmmoComponent::SetMaxAmmo(int32 NewMaxAmmo)
+{
+	MaxAmmo = NewMaxAmmo;
+	return MaxAmmo;
+}
+
+int32 UAmmoComponent::SetMaxReserveAmmo(int32 NewMaxReserveAmmo)
+{
+	MaxReserveAmmo = NewMaxReserveAmmo;
+	return MaxReserveAmmo;
+}
+
+float UAmmoComponent::GetReloadTimeLeftPercent()
+{
+	return GetRemainingReloadTime()/ReloadDuration;
+}
+
+bool UAmmoComponent::IsAmmoDepleted()
+{
+	return Ammo <= 0;
+}
+
+bool UAmmoComponent::IsReserveAmmoDepleted()
+{
+	return ReserveAmmo <= 0;
+}
+
+float UAmmoComponent::GetAmmoPercent()
+{
+	return Ammo/MaxAmmo;
+}
+
+float UAmmoComponent::GetRemainingReloadTime()
+{
+	if(IsReloading() && GetWorld()->GetTimerManager().IsTimerActive(ReloadingHandle))
+	{
+		return GetWorld()->GetTimerManager().GetTimerRemaining(ReloadingHandle);
+	}
+	else
+	{
+		return 0.f;
+	}
+}
+
+bool UAmmoComponent::NeedsAndCanReload()
+{
+	return NeedsToReload() && CanReload();
+}
+
+FString UAmmoComponent::GetDebugInfo()
+{
+	return "" + FString::FromInt(Ammo) + " / " + FString::FromInt(ReserveAmmo) + ", Reloading: " + (bIsReloading ? "true" : "false");
+}
+
+void UAmmoComponent::ResetAmmo()
+{
+	SetAmmo(MaxAmmo);
+	SetReserveAmmo(MaxReserveAmmo);
+}
+
+float UAmmoComponent::SetReloadDuration(float NewReloadDuration)
+{
+	ReloadDuration = NewReloadDuration;
+	return ReloadDuration;
+}
+
+bool UAmmoComponent::NeedsToReload()
+{
+	return Ammo < MaxAmmo;
+}
+
+bool UAmmoComponent::CanReload()
+{
+	return (ReserveAmmo > 0 && !IsReloading());
+}
+
+void UAmmoComponent::StartReloading()
+{
+	if(!IsReloading())
+	{
+		bIsReloading = true;
+		GetWorld()->GetTimerManager().SetTimer(ReloadingHandle, this, &UAmmoComponent::FinishReloading, ReloadDuration);
+		OnReloadStart.Broadcast();
+	}
+}
+
+void UAmmoComponent::FinishReloading()
+{
+	//if(IsReloading())
+	if(bIsReloading)
+	{
+		if(ReserveAmmo >= (MaxAmmo - Ammo))
+		{
+			SetReserveAmmo(ReserveAmmo-(MaxAmmo-Ammo));
+			SetAmmo(MaxAmmo);
+		}
+		else
+		{
+			SetAmmo(ReserveAmmo);
+			SetReserveAmmo(0);
+		}
+		bIsReloading = false;
+		OnReloadComplete.Broadcast();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Called without bIsReloading being true"))
+	}
+}
---
+++ AmmoComponent.h
@@ -0,0 +1,181 @@
+/*
+Implementation:
+
+GAMENAME_API needs to be set to the name that you chose
+When you created the project
+
+eg. SHOOTERGAME_API - Make sure its in capitals
+
+
+Usage:
+
+Put This In Your Gun Class File (Gun.h)
+	UAmmoComponent* AmmoComponent;
+
+Put This In Your Gun Constructor (Gun.cpp)
+	AmmoComponent = CreateDefaultSubobject<UAmmoComponent>(this, TEXT("AmmoComponent"));
+
+*/
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "AmmoComponent.generated.h" // May need to regenerate visual studio files after adding the files to your project
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FAmmoComponentDelegate);
+
+
+UCLASS( Config=Game, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class GAMENAME_API UAmmoComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UAmmoComponent();
+
+	// Called when the game starts - TODO: Do i need this?
+	virtual void BeginPlay() override;
+
+	// Handles all ammo for you
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		bool Fire();
+
+	// Handles all reloading for you
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		void Reload();
+
+	// Checks if you are reloading
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		bool IsReloading();
+
+	// Stops and resets reloading
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		void CancelReload();
+
+	// Simple Function To Subtract Ammo and Clamps between 0 - MaxAmmo
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		int32 TakeAmmo(int32 Amount);
+
+	// Simple Function To Add Ammo and Clamps between 0 - MaxAmmo
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		int32 GiveAmmo(int32 Amount);
+
+	// Simple Function To Subtract Reserve Ammo and Clamps between 0 - MaxReserveAmmo
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		int32 TakeReserveAmmo();
+
+	// Simple Function To Add Reserve Ammo and Clamps between 0 - MaxReserveAmmo
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		int32 GiveReserveAmmo(int32 Amount);
+
+	// Simple Function That Sets Ammo To NewAmmo Param, Clamped between 0 - MaxAmmo
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		int32 SetAmmo(int32 NewAmmo);
+
+	// Simple Function That Sets Reserve To NewReserveAmmo Param, Clamped between 0 - MaxReserveAmmo
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		int32 SetReserveAmmo(int32 NewReserveAmmo);
+
+	// Simple Function That Sets MaxAmmo To NewMaxAmmo
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		int32 SetMaxAmmo(int32 NewMaxAmmo);
+
+	// Simple Function That Sets MaxReserveAmmo To NewMaxReserveAmmo
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		int32 SetMaxReserveAmmo(int32 NewMaxReserveAmmo);
+
+	// Simple Function That Returns 0-1 float value for (TimeLeft/ReloadDuration)
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		float GetReloadTimeLeftPercent();
+
+	// Simple Function That Returns True If Ammo <= 0
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		bool IsAmmoDepleted();
+
+	// Simple Function That Returns True If ReserveAmmo <= 0
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		bool IsReserveAmmoDepleted();
+
+	// Simple Function That Returns 0-1 float value for (Ammo/MaxAmmo)
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		float GetAmmoPercent();
+
+	// Simple Function That Returns float value for Time Left in Reload Timer ( 0 if timer doesn't exist )
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		float GetRemainingReloadTime();
+
+	// Function that returns true if both NeedsToReload() and CanReload() are true
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		bool NeedsAndCanReload();
+
+	// Returns useful Debug Info That Can Be Printed (Ammo / ReserveAmmo, Reloading: bIsReloading)
+	// Could Be Improved To Return Things Such As Max Values
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		FString GetDebugInfo();
+
+	// Sets Variables to their defined max value
+	UFUNCTION(BlueprintCallable, Category = "AmmoComponent")
+		void ResetAmmo();
+
+	// Delegate that is called from SetAmmo() when Ammo <= 0
+	UPROPERTY(BlueprintAssignable, Category = "AmmoComponent")
+		FAmmoComponentDelegate OnAmmoDepleted;
+
+	// Delegate that is called from SetReserveAmmo() when ReserveAmmo <= 0
+	UPROPERTY(BlueprintAssignable, Category = "AmmoComponent")
+		FAmmoComponentDelegate OnReserveDepleted;
+
+	// Delegate that is called from StartReloading()
+	UPROPERTY(BlueprintAssignable, Category = "AmmoComponent")
+		FAmmoComponentDelegate OnReloadStart;
+
+	// Delegate that is called from FinishReloading();
+	UPROPERTY(BlueprintAssignable, Category = "AmmoComponent")
+		FAmmoComponentDelegate OnReloadComplete;
+
+	// Variables
+	UPROPERTY(BlueprintReadWrite, Category = "Ammo")
+		int32 Ammo = 30;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Ammo")
+		int32 MaxAmmo = 30;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Ammo")
+		int32 ReserveAmmo = 90;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Ammo")
+		int32 MaxReserveAmmo = 90;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Ammo")
+		int32 BulletsPerShot = 1;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Reload")
+		float ReloadDuration = 2.f;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Reload")
+		bool FiringWithNoBulletsStartsReload = false;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Reload")
+		bool bShootingInteruptsReload = false;
+
+private:
+
+	// Setters
+	float SetReloadDuration(float NewReloadDuration);
+
+	// Helper Functions - You can expose these to BP if you want
+	bool NeedsToReload();
+	bool CanReload();
+
+	// Shouldn't need to call these directly
+	void StartReloading();
+	void FinishReloading();
+
+	// Shouldn't need to touch this ever
+	bool bIsReloading = false;
+
+	// Shouldn't need to touch this ever
+	FTimerHandle ReloadingHandle;
+};
